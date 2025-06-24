8f5aa655a8f21ee46359faac7c9306f9682e7bda
rizaldyip10 <rizaldyimanputra@gmail.com>
2025-06-24 16:34:45 +0700

    feat: changing address style and adding google maps location in contact page

--- src/components/features/contact/contact-address-list.tsx
+++ src/components/features/contact/contact-address-list.tsx
@@ -8,39 +8,51 @@ import { easeIn } from "motion";
 const ContactAddressList = () => {
   return (
     <AnimationWrapper
-      classname="w-full lg:border-l border-gray-400 py-10 lg:pl-[60px] flex flex-col gap-4 justify-between"
+      classname="w-full 2xl:w-[1440px] grid grid-cols-1 lg:grid-cols-2 relative px-8 lg:px-16 space-y-6"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.7, ease: easeIn }}
     >
-      <h6 className="text-2xl font-semibold text-[#A22C51]">
-        Get in touch with us
-      </h6>
-      <div className="flex items-start lg:items-center gap-2 text-[#999999]">
-        <MarkerIcon className="text-lg font-bold" />
-        <p className="max-w-2xs">
-          GKM Green Tower Floor 20 Jl. TB Simatupang Kav. 89-G, Jakarta Selatan
-          (12520) - INDONESIA
-        </p>
+      <div className="w-full lg:px-8 space-y-5">
+        <div className="flex items-start lg:items-center gap-2 text-[#999999]">
+          <MarkerIcon className="text-lg font-bold" />
+          <p className="max-w-2xs xl:max-w-lg">
+            GKM Green Tower Floor 20 Jl. TB Simatupang Kav. 89-G, Jakarta
+            Selatan (12520) - INDONESIA
+          </p>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.7045493097075!2d106.83410317499107!3d-6.302496493686701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1563912fed9%3A0xf4fa5584742669b6!2sJakarta%20International%20Law%20Office%20(JILO)!5e0!3m2!1sen!2sid!4v1750754510406!5m2!1sen!2sid"
+          loading="lazy"
+          className="w-full h-80"
+        />
       </div>
-      <div className="flex items-start lg:items-center gap-2 text-[#999999]">
-        <MarkerIcon className="text-lg font-bold" />
-        <p className="max-w-2xs">
-          Jl. Waru Nomor 8-A, RT. 09, RW. 03, Gedong, Pasar Rebo, Jakarta Timur
-          (13760) - INDONESIA
-        </p>
+      <div className="w-full lg:px-8 space-y-5">
+        <div className="flex items-start lg:items-center gap-2 text-[#999999]">
+          <MarkerIcon className="text-lg font-bold" />
+          <p className="max-w-2xs xl:max-w-lg">
+            Jl. Waru Nomor 8-A, RT. 09, RW. 03, Gedong, Pasar Rebo, Jakarta
+            Timur (13760) - INDONESIA
+          </p>
+        </div>
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d310.4865985113928!2d106.85828933789823!3d-6.293263566134743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f30068af2779%3A0x145806c6fd3d2f03!2sDPP%20DePA-RI%20Dewan%20Pimpinan%20Pusat!5e0!3m2!1sen!2sid!4v1750757564919!5m2!1sen!2sid" 
+          className="w-full h-80"
+        />
       </div>
-      <div className="flex items-center gap-2 text-[#999999]">
-        <PhoneIcon className="w-[18px] h-[18px]" />
-        <p className="max-w-2xs">+62 21 2949 0519</p>
-      </div>
-      <div className="flex items-center gap-2 text-[#999999]">
-        <PhoneIcon className="w-[18px] h-[18px]" />
-        <p className="max-w-2xs">+62 8778 6151</p>
-      </div>
-      <div className="flex items-center gap-2 text-[#999999]">
-        <EnvelopeIcon className="text-lg font-bold" />
-        <p className="max-w-2xs">info@jilolaw.com</p>
+      <div className="w-full lg:col-span-2 flex flex-col md:flex-row items-center justify-center gap-4">
+        <div className="flex items-center gap-2 text-[#999999]">
+          <PhoneIcon className="w-[18px] h-[18px]" />
+          <p className="max-w-2xs">+62 21 2949 0519</p>
+        </div>
+        <div className="flex items-center gap-2 text-[#999999]">
+          <PhoneIcon className="w-[18px] h-[18px]" />
+          <p className="max-w-2xs">+62 8778 6151</p>
+        </div>
+        <div className="flex items-center gap-2 text-[#999999]">
+          <EnvelopeIcon className="text-lg font-bold" />
+          <p className="max-w-2xs">info@jilolaw.com</p>
+        </div>
       </div>
     </AnimationWrapper>
   );
--- src/components/features/contact/contact-address-title.tsx
+++ src/components/features/contact/contact-address-title.tsx
@@ -6,21 +6,13 @@ import AnimationWrapper from "@/components/wrappers/animation-wrapper";
 const ContactAddressTitle = () => {
   return (
     <AnimationWrapper
-        classname="w-full h-80 flex items-center justify-center relative"
+        classname="w-full h-full flex flex-col items-center justify-center relative gap-2"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.7, ease: easeIn }}
     >
-      <div
-        className="absolute inset-0 grayscale opacity-30"
-        style={{
-          backgroundImage: "url(/jilo-logo-small.png)",
-          backgroundSize: "60% auto",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center center",
-        }}
-      />
-      <h5 className="text-4xl font-semibold relative z-10">Our Addresses</h5>
+      <h2 className="text-3xl text-[#E1BC1C] font-bold">Our Address</h2>
+      <h4 className="text-xl font-medium text-[#A22C51]">Get in touch with us</h4>
     </AnimationWrapper>
   );
 };
